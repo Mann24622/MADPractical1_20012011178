0554ff25d72b334f4fbee76b71e3705a948ad30e
RajSukhadiya21 <rajsukhadiya387@gmail.com>
2022-08-23 17:43:49 +0530

    This is practical-1

---
+++ src/main/kotlin/p1.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    var x:Int=22
+    println("$x")
+    var z=39.5f
+    println("$z")
+    var y='R'
+    println("$y")
+    var a="Mann"
+    println("$a")
+    var b=true
+    println("$b")
+    var c=96.36
+    println("$c")
+    var d=6424668464
+    println("$d")
+    var e:Short=-2
+    println("$e")
+    var f:Byte= 127
+    println("$f")
+
+}
